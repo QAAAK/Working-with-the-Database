0a57e40a0638324d81896771d2f80c55946cb6aa
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-04-11 11:33:30 +0300

    Update numeric_to_int.sql

--- SQL QUERY for Greenplum, Oracle, MS SQL/Greenplum/numeric_to_int.sql
+++ SQL QUERY for Greenplum, Oracle, MS SQL/Greenplum/numeric_to_int.sql
@@ -1,13 +1,18 @@
-select table_name,
-           column_name,
-           data_type || ' (' || coalesce(numeric_precision::text, '') || ','  || coalesce(numeric_scale::text, 'безразмерно') || ')'  as data_type,
-           case
-                   when numeric_precision <= 5 and numeric_scale = 0 then 'int2'
-                   when numeric_precision <= 10 and numeric_scale = 0 then 'int4'
-                   when numeric_precision <= 19 and numeric_scale = 0 then 'int8'
-           else 'эквивалентно текущему типу данных'
-           end as numeric_to_int
+with t as(select table_name,
+   column_name,
+   data_type || ' (' || coalesce(numeric_precision::text, '') || ','  || coalesce(numeric_scale::text, 'безразмерно') || ')'  as data_type,
+   case
+when numeric_precision <= 5 and numeric_scale = 0 then 'int2'
+when numeric_precision <= 10 and numeric_scale = 0 then 'int4'
+when numeric_precision <= 19 and numeric_scale = 0 then 'int8'
+   else null
+   end as numeric_to_int
 from information_schema.columns
 where table_schema = 'gl'
 and   data_type = 'numeric'
-and   table_name in (select trg_tbl_name from meta_info.ee_gl_md)
+and   table_name in (select trg_tbl_name from meta_info.ee_gl_md))
+
+
+
+select 'ALTER TABLE gl.'|| table_name || ' ALTER COLUMN ' || column_name || ' TYPE ' || numeric_to_int ||';'  from t
+where numeric_to_int is not null
