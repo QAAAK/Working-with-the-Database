09a020f9ef06fd7c21a98548de4ae419c6a015f2
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-18 11:57:05 +0300

    Update os.py

--- Excel_to_scv/os.py
+++ Excel_to_scv/os.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import os
 
-
-
-def read_excel_to_csv():
-    
+def read_excel_to_csv():    
     arr = []
     
     for file in os.listdir(r'C:\Users\Santalov-dv\Desktop\Excel'):
         arr.append(file)
 
-    for i in arr:
-        
+    for i in arr:        
         try:
             read_file = pd.read_excel (r'C:\Users\Santalov-dv\Desktop\Excel\{i}'.format(i)) 
             read_file.to_csv (r"C:\Users\Santalov-dv\Desktop\csv\test.csv",  
@@ -19,9 +15,7 @@ def read_excel_to_csv():
                                 header=True) 
         except:
             print( "Неверное имя файла")
-
    
     return 'Passed!'
-
             
 read_excel_to_csv()
