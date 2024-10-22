3c5e38cf5d6c4b95302ca3bb53ef219a764d76c3
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-10-22 14:03:53 +0300

    Update Oscar_win_bar_graph.py

--- matplotlib_graph/Oscar_win_bar_graph.py
+++ matplotlib_graph/Oscar_win_bar_graph.py
@@ -3,12 +3,8 @@ from matplotlib import pyplot as plt
 movies = ['Anny Hall', 'Ben-Gur', 'Casablanka','Gandi', 'Westside Story']
 count_oscar = [5,11,3,8,10]
 
-
 plt.bar(movies, count_oscar, color = 'green')
-    
-
 plt.title('Movies')
 plt.ylabel('Oscar')
-
 
 plt.show()
