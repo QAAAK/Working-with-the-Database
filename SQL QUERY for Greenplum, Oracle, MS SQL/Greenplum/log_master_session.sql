017b2dca34e75694a1e31a61c0654be59f6f164c
Dmitry Santalov <dmitsantalov@yandex.ru>
2025-01-14 11:51:02 +0300

    Update log_master_session.sql

--- SQL QUERY for Greenplum, Oracle, MS SQL/Greenplum/log_master_session.sql
+++ SQL QUERY for Greenplum, Oracle, MS SQL/Greenplum/log_master_session.sql
@@ -1,3 +1,3 @@
-select logtime , loguser , logdatabase , logpid , logsession , logsessiontime , logmessage , logdetail 
+select logtime, loguser, logdatabase, logpid, logsession, logsessiontime, logmessage, logdetail 
 from gp_toolkit."__gp_log_master_ext"
 order by logsessiontime desc
