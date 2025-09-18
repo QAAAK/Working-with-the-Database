4c4630d0014ddabf7046a598a40ef9e5b88de4af
Dmitry Santalov <dmitsantalov@yandex.ru>
2025-09-18 10:06:54 +0300

    cycle replacement

--- functions/load_json_to_tables.sql
+++ functions/load_json_to_tables.sql
@@ -1,8 +1,22 @@
-CREATE OR REPLACE FUNCTION load_json_to_tables(json_array jsonb)
-RETURNS VOID AS $$
+-- DROP FUNCTION public.load_json_to_tables(jsonb);
+
+CREATE OR REPLACE FUNCTION public.load_json_to_tables(json_array jsonb)
+	RETURNS void
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
 DECLARE
     json_element jsonb;
+    item jsonb;
+    offer jsonb;
+    result_text text := 'PASSED';
 BEGIN
+
+	/* 
+	Циклом обходим элементы массива (в массиве будет несколько json пакетов) и загружаем первую часть orders
+	*/
+	
     FOR json_element IN SELECT * FROM jsonb_array_elements(json_array)
     LOOP
         INSERT INTO orders(
@@ -25,21 +39,25 @@ BEGIN
         VALUES (
             (json_element ->> 'id')::uuid,
             json_element ->> 'type',
-            (json_element ->> 'time')::timestamptz,
+            (json_element ->> 'time')::timestamp,
             (json_element -> 'data' -> 'order' ->> 'orderId'),
             (json_element -> 'data' -> 'order' ->> 'realmId'),
             (json_element -> 'data' -> 'order' ->> 'requestId'),
             (json_element -> 'data' -> 'order' -> 'originUser' ->> 'phone'),
             (json_element -> 'data' -> 'order' -> 'customer' ->> 'phone'),
-            (json_element -> 'data' -> 'order' ->> 'orderDate')::timestamptz,
-            (json_element -> 'data' -> 'order' ->> 'completeDate')::timestamptz,
+            (json_element -> 'data' -> 'order' ->> 'orderDate')::timestamp,
+            (json_element -> 'data' -> 'order' ->> 'completeDate')::timestamp,
             (json_element -> 'data' -> 'order' -> 'total' ->> 'amount')::int,
             (json_element -> 'data' -> 'order' -> 'total' ->> 'currency'),
             (json_element -> 'data' -> 'order' ->> 'state'),
             (json_element -> 'data' -> 'order' -> 'epCheckout' ->> 'id'),
             (json_element -> 'data' -> 'order' -> 'epProductOrder' ->> 'id')
         );
-        
+
+		/* 
+		Два вложенных цикла загружают две вложенные конструкции json-пакета
+		*/
+
         FOR item IN SELECT * FROM jsonb_array_elements(json_element -> 'data' -> 'order' -> 'items')
         LOOP
             INSERT INTO items(
@@ -57,7 +75,7 @@ BEGIN
                 item ->> 'result'
             );
         END LOOP;
-        
+
         FOR offer IN SELECT * FROM jsonb_array_elements(json_element -> 'data' -> 'order' -> 'epProductOffers')
         LOOP
             INSERT INTO ep_product_offers(
@@ -71,4 +89,7 @@ BEGIN
         END LOOP;
     END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+
+$$
+EXECUTE ON ANY;
+
