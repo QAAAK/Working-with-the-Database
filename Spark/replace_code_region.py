ff39ba95c975a4472011f3c352efb059a2bacc61
Dmitry Santalov <dmitsantalov@yandex.ru>
2026-01-12 11:26:03 +0300

    Create replace_code_region.py for phone number conversion
    Added a script to convert phone numbers in a Spark DataFrame.

---
+++ Spark/replace_code_region.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
+
+# Создаем SparkSession
+spark = SparkSession.builder \
+    .appName('Phone number conversion') \
+    .config('spark.ui.showConsoleProgress', True) \
+    .config('spark.sql.execution.arrow.pyspark.enabled', True) \
+    .config('spark.driver.memory', '2g') \
+    .config('spark.driver.cores', 2) \
+    .config('spark.executor.cores', 5) \
+    .config('spark.executor.memory', '16g') \
+    .config('spark.submit.deploymode', 'client') \
+    .config('spark.dynamicAllocation.enabled', True) \
+    .config('spark.dynamicAllocation.minExecutors', 5) \
+    .config('spark.dynamicAllocation.maxExecutors', 8) \
+    .config('spark.dynamicAllocation.executorIdleTimeout', '60s') \
+    .config('spark.yarn.queue', 'fob2b') \
+    .getOrCreate()
+
+df = spark.table('table_name')
+
+def convert_phone_number(phone):
+    if phone.startswith('8'):
+        return '+7' + phone[1:]
+    return phone
+
+udf_convert_phone_number = udf(convert_phone_number, StringType())
+df = df.withColumn('phone', udf_convert_phone_number(col('phone')))
+
+spark.stop()
